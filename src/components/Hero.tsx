1dde2164805f74b2d35fde8e1693f3ca425b9460
Keshav Garg <141502522+keshavgarg24@users.noreply.github.com>
2024-05-30 23:45:44 +0530

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -71,7 +71,7 @@ export function Hero() {
       />
       <div className="p-4 max-w-7xl mx-auto relative z-10 w-full pt-20 md:pt-0">
         <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
-          {typedGreeting} <br /> I am KESHAV GARG
+          {typedGreeting} <br /> I am <a href="https://linktr.ee/keshavgarg24">KESHAV GARG</a>
         </h1>
         <p className="mt-4 font-normal text-base text-neutral-300 max-w-lg text-center mx-auto">
           Software developer from India, enthusiastic about web development and data science.
